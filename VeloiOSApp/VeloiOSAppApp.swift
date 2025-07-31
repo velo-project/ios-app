a578912b7ee9a268d6adaeab6c7dc39112dd73b1
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-07-30 22:59:53 -0300

    Initial Commit

---
+++ VeloiOSApp/VeloiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeloiOSAppApp.swift
+//  VeloiOSApp
+//
+//  Created by Gabriel Araújo Lima on 30/07/25.
+//
+
+import SwiftUI
+
+@main
+struct VeloiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
