fb8b326f7a6005606cfaea6e6b94bc002114458f
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-10-22 14:27:27 -0300

    chore: show user current location

--- VeloiOSApp/Services/Maps/GoogleMapsView.swift
+++ VeloiOSApp/Services/Maps/GoogleMapsView.swift
@@ -1,35 +1,50 @@
-//
-//  GoogleMapsView.swift
-//  VeloiOSApp
-//
-//  Created by Gabriel Araújo Lima on 16/10/25.
-//
-
+// GoogleMapsView.swift
 import SwiftUI
 import GoogleMaps
+import CoreLocation
 
 struct GoogleMapsView: UIViewRepresentable {
     
-    private let zoom: Float = 15.0
+    private let zoom: Float = 17.0
     
     @Binding var lastKnowLocation: CLLocationCoordinate2D?
-
-    func makeUIView(context: Self.Context) -> GMSMapView {
-        
-        guard let lastKnowLocation = lastKnowLocation else {
-            return GMSMapView()
-        }
-
-        let camera = GMSCameraPosition.camera(withTarget: lastKnowLocation, zoom: zoom)
+    @State private var hasAnimatedToUserLocation = false
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> GMSMapView {
+        let camera = GMSCameraPosition.camera(
+            withLatitude: lastKnowLocation?.latitude ?? -23.5505,
+            longitude: lastKnowLocation?.longitude ?? -46.6333,
+            zoom: 15.0
+        )
         let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        
+        mapView.delegate = context.coordinator
+        
         return mapView
     }
-
+    
     func updateUIView(_ mapView: GMSMapView, context: Context) {
-        guard let lastKnowLocation = lastKnowLocation else {
-            return
+        if let userLocation = lastKnowLocation, !hasAnimatedToUserLocation {
+            mapView.animate(to: GMSCameraPosition.camera(withTarget: userLocation, zoom: zoom))
         }
+    }
+    
+    class Coordinator: NSObject, GMSMapViewDelegate {
+        var parent: GoogleMapsView
         
-        mapView.animate(toLocation: lastKnowLocation)
+        init(_ parent: GoogleMapsView) {
+            self.parent = parent
+        }
+    
+        func mapView(_ mapView: GMSMapView, idleAt position: GMSCameraPosition) {
+            if !parent.hasAnimatedToUserLocation {
+                parent.hasAnimatedToUserLocation = true
+            }
+        }
     }
 }
