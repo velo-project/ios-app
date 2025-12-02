ebb8ef9532e59a83ca19f56eb83e4e631d4f1c64
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-12-02 15:58:11 -0300

    fix: Manage loading state locally in password recovery views

--- VeloiOSApp/Features/Auth/View/ForgotPasswordView.swift
+++ VeloiOSApp/Features/Auth/View/ForgotPasswordView.swift
@@ -11,57 +11,59 @@ struct ForgotPasswordView: View {
     @StateObject private var viewModel = ForgotPasswordViewModel()
     @StateObject private var sheetStore = SheetStore.shared
     
-    @Binding var isLoading: Bool
+    @State private var isLoading: Bool = false
     
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 50) {
-                Image("AppLogo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: 80)
-                    .padding(.horizontal, .none)
-                    .padding(.top, 50)
-                VStack(alignment: .leading) {
-                    Text("esqueceu sua senha?")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                    Text("insira seu e-mail para\nrecuperar sua senha.")
-                }
-                VStack(spacing: 20) {
-                    VStack {
-                        VeloTextField(
-                            text: $viewModel.email,
-                            placeholder: "email",
-                            icon: "envelope")
+        ZStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 50) {
+                    Image("AppLogo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 80)
+                        .padding(.horizontal, .none)
+                        .padding(.top, 50)
+                    VStack(alignment: .leading) {
+                        Text("esqueceu sua senha?")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Text("insira seu e-mail para\nrecuperar sua senha.")
                     }
-                    
-                    VeloButton {
-                        Text("enviar")
-                    } action: {
-                        if viewModel.email != "" {
-                            isLoading = true
+                    VStack(spacing: 20) {
+                        VStack {
+                            VeloTextField(
+                                text: $viewModel.email,
+                                placeholder: "email",
+                                icon: "envelope")
+                        }
+                        
+                        VeloButton {
+                            Text("enviar")
+                        } action: {
+                            if viewModel.email != "" {
+                                isLoading = true
+                                await viewModel.forgotPassword()
+                                sheetStore.sheet = .recoveryPasswordConfirmation
+                                isLoading = false
+                            }
+                        }
+                        
+                        Button(action: {
                             dismiss()
-                            await viewModel.forgotPassword()
-                            sheetStore.sheet = .recoveryPasswordConfirmation
+                            sheetStore.sheet = .login
+                        }) {
+                            Text("Lembrou a senha? Faça login")
                         }
                     }
-                    
-                    Button(action: {
-                        dismiss()
-                        sheetStore.sheet = .login
-                    }) {
-                        Text("Lembrou a senha? Faça login")
-                    }
                 }
+                .padding()
+                Spacer()
             }
-            .padding()
-            Spacer()
+            if isLoading {
+                Color.black.opacity(0.5).edgesIgnoringSafeArea(.all)
+                ProgressView()
+            }
         }
-        .onAppear {
-            isLoading = false
-        }
-    }
 }
--- VeloiOSApp/Features/Auth/View/RecoveryPasswordConfirmationView.swift
+++ VeloiOSApp/Features/Auth/View/RecoveryPasswordConfirmationView.swift
@@ -11,62 +11,65 @@ struct RecoveryPasswordConfirmationView: View {
     @StateObject private var viewModel = RecoveryPasswordConfirmationViewModel()
     @StateObject private var sheetStore = SheetStore.shared
     
-    @Binding var isLoading: Bool
+    @State private var isLoading: Bool = false
     
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 50) {
-                Image("AppLogo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: 80)
-                    .padding(.horizontal, .none)
-                    .padding(.top, 50)
-                VStack(alignment: .leading) {
-                    Text("crie uma nova senha")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                    Text("insira o código enviado para o seu e-mail e crie uma nova senha.")
-                }
-                VStack(spacing: 20) {
-                    VStack {
-                        VeloTextField(
-                            text: $viewModel.code,
-                            placeholder: "código",
-                            icon: "number")
-                        
-                        VeloTextField(
-                            text: $viewModel.password,
-                            placeholder: "nova senha",
-                            icon: "lock",
-                            isSecure: true)
+        ZStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 50) {
+                    Image("AppLogo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 80)
+                        .padding(.horizontal, .none)
+                        .padding(.top, 50)
+                    VStack(alignment: .leading) {
+                        Text("crie uma nova senha")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Text("insira o código enviado para o seu e-mail e crie uma nova senha.")
                     }
-                    
-                    VeloButton {
-                        Text("salvar")
-                    } action: {
-                        if viewModel.code != "" && viewModel.password != "" {
-                            isLoading = true
+                    VStack(spacing: 20) {
+                        VStack {
+                            VeloTextField(
+                                text: $viewModel.code,
+                                placeholder: "código",
+                                icon: "number")
+                            
+                            VeloTextField(
+                                text: $viewModel.password,
+                                placeholder: "nova senha",
+                                icon: "lock",
+                                isSecure: true)
+                        }
+                        
+                        VeloButton {
+                            Text("salvar")
+                        } action: {
+                            if viewModel.code != "" && viewModel.password != "" {
+                                isLoading = true
+                                await viewModel.recoveryPasswordConfirmation()
+                                isLoading = false
+                                dismiss()
+                            }
+                        }
+                        
+                        Button(action: {
                             dismiss()
-                            await viewModel.recoveryPasswordConfirmation()
+                            sheetStore.sheet = .login
+                        }) {
+                            Text("Lembrou a senha? Faça login")
                         }
                     }
-                    
-                    Button(action: {
-                        dismiss()
-                        sheetStore.sheet = .login
-                    }) {
-                        Text("Lembrou a senha? Faça login")
-                    }
                 }
+                .padding()
+                Spacer()
             }
-            .padding()
-            Spacer()
+            if isLoading {
+                Color.black.opacity(0.5).edgesIgnoringSafeArea(.all)
+                ProgressView()
+            }
         }
-        .onAppear {
-            isLoading = false
-        }
-    }
 }
